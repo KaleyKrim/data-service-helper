c1df5b492380d7c40accc939ddfd458aef08e973
Kaley Kriminger <kaley.kriminger@gmail.com>
2019-09-27 11:43:46 +0900

    execute statement

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as AWS from "aws-sdk";
+import { RDSDataService } from "aws-sdk";
 
 export class RDSDataAPI {
-    public RDSDataService: AWS.RDSDataService;
+    public RDSDataService: RDSDataService;
     public baseParams: { resourceArn: string; secretArn: string };
     constructor(accessKeyId: string, secretAccessKey: string, region: string, resourceArn: string, secretArn: string) {
-        this.RDSDataService = new AWS.RDSDataService({
+        this.RDSDataService = new RDSDataService({
             accessKeyId,
             secretAccessKey,
             region,
@@ -16,19 +16,19 @@ export class RDSDataAPI {
         };
     }
 
-    public async beginTransaction(dbName: string): Promise<string> {
+    public async beginTransaction(database: string): Promise<string> {
         return (await this.RDSDataService.beginTransaction({ ...this.baseParams, database: dbName }).promise()).transactionId;
     }
 
-    public async commitTransaction() {
-        
+    public async commitTransaction(transactionId: string): Promise<RDSDataService.TransactionStatus> {
+        return (await this.RDSDataService.commitTransaction({ ...this.baseParams, transactionId }).promise()).transactionStatus;
     }
 
-    public async rollBack() {
-
+    public async rollbackTransaction(transactionId: string): Promise<RDSDataService.TransactionStatus> {
+        return (await this.RDSDataService.rollbackTransaction({ ...this.baseParams, transactionId }).promise()).transactionStatus;
     }
 
-    public async executeStatement() {
-
+    public async executeStatement(sql: string, options?: { database?: string; transactionId?: string; }): Promise<RDSDataService.Types.ExecuteStatementResponse> {
+        return (await this.RDSDataService.executeStatement({ ...this.baseParams, sql, ...options }).promise());
     }
 }
