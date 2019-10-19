4e10b427e1eb4eeb4657f7c4743030480e567356
Kaley Kriminger <kaley.kriminger@gmail.com>
2019-10-19 15:02:20 +0900

    wip

--- test/src/dataWrapper.ts
+++ test/src/dataWrapper.ts
@@ -3,8 +3,5 @@ import RDSDataServiceClient from "../../build";
 
 ava.serial("successfully calls the rds data api", async t => {
     const database = "test";
-    const rdsDataApi = new RDSDataServiceClient("aaa", "sss");
-    // const transactionId = await rdsDataApi.beginTransaction(database);
-    // const insert = await rdsDataApi.executeStatement("", { transactionId, database });
-    // const commitTransaction = await rdsDataApi.commitTransaction(transactionId);
+    const rdsDataApi = new RDSDataServiceClient();
 });
